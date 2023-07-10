06fb2f94f913ff53a7db6992aa345905d8e5e12a
Esther Nyangau Onyando <eonyando@safaricom.co.ke>
2023-07-10 15:26:40 +0300

    Working on the nav bar and Home Page

---
+++ src/mock/profile.js
@@ -0,0 +1,24 @@
+export const SOCIAL_LINKS = [
+    {
+      link: 'https://twitter.com/Esther_Onyando',       
+      icon: 'akar-icons:twitter-fill',        
+    },
+    {
+      link: 'https://github.com/estheronyando',
+      icon: 'akar-icons:github-fill',
+    },
+    {
+      link: 'https://linkedin.com/in/esther-onyando',
+      icon: 'akar-icons:linkedin-fill',
+    },
+  ];
+  
+  export const HERO_TITLES = ['Esther Nyang\'au Onyando', 'a Full-stack Developer', 'a Javascript Enthusiast'];
+  
+  export const aboutParagraph =
+    'Highly motivated and enthusiastic Full Stack Developer with experience in designing, developing and maintaining web applications using technologies such as JavaScript, React, Node.js, Java. ';
+  
+  export const contactEmail = 'esthermariachana@gmail.com';
+  
+  export const documentTitle = 'Hello I am Esther Nyang,au Onyando | Full Stack Developer | Javascript Enthusiast'
+  
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,21 @@
-import '@/styles/globals.css'
+import PropTypes from 'prop-types';
+// provider
+import { ScrollProvider } from '../context/ScrollContext';
+// style
+import '../styles/globals.css';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <ScrollProvider>
+      <Component {...pageProps} />
+    </ScrollProvider>
+  );
 }
+
+// ---------------------------------------
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType,
+  pageProps: PropTypes.object,
+  emotionCache: PropTypes.object,
+};
